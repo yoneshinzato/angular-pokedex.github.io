6d036314f781f517b59ec49933bb31b4f2fbdd29
Yone <chibariyo@gmail.com>
2022-08-08 00:32:22 -0300

    criando o consumo da api de pokemons

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,10 +15,18 @@ export class PokeApiService {
     return this.http.get<any>(this.url).pipe(
       tap(res => res),
       tap(res => {
-        console.log(res);
+        res.results.map( (resPokemons: any) => {
+          this.apiGetPokemons(resPokemons.url)
+          .subscribe(
+            res => resPokemons.status = res
+        )
+        })
 
       })
     )
   }
-
+  public apiGetPokemons(url: string): Observable<any> {
+    return this.http.get<any>(url).pipe(
+      map(res => res))
+  }
 }
