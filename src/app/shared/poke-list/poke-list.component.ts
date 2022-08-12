049d6df1fddd7c43d29ad7a2b59d42c8d82d433e
yoneshinzato <chibariyo@gmail.com>
2022-08-12 00:46:36 -0300

    filtrando pokemons

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -7,6 +7,7 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
   styleUrls: ['./poke-list.component.scss']
 })
 export class PokeListComponent implements OnInit {
+  private setAllPokemons: any;
   public getAllPokemons: any;
   constructor(
     private pokeApiService: PokeApiService
@@ -15,10 +16,17 @@ export class PokeListComponent implements OnInit {
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe(
       res => {
-        this.getAllPokemons = res.results
-        
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
       }
     )
+  }
+
+  getSearch(value: string) {
+    const filter = this.setAllPokemons.filter((res: any) => {
+      return !res.name.indexOf(value.toLowerCase())
+    })
+    this.getAllPokemons = filter;
   }
 
 }
