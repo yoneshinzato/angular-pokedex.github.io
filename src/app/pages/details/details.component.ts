5f849eef3ae6b0b01e8fbad3aae4f86e82e81b19
yoneshinzato <chibariyo@gmail.com>
2022-08-18 11:39:36 -0300

    forkjoin recuperar nome e caracteristicas do pokemon

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon'
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species'
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private pokeApiService: PokeApiService) { }
 
   ngOnInit(): void {
+    this.pokemon
+  }
+
+  get pokemon() {
+    const id = this.activatedRoute.snapshot.params['id']
+    const pokemon = this.pokeApiService.apiGetPokemon(`${this.urlPokemon}/${id}`)
+    const name = this.pokeApiService.apiGetPokemon(`${this.urlName}/${id}`)
+    return forkJoin([pokemon, name]).subscribe(
+      res => {
+        console.log(res)
+      }
+    )
+    return console.log(id)
   }
 
 }
--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -16,7 +16,7 @@ export class PokeApiService {
       tap(res => res),
       tap(res => {
         res.results.map( (resPokemons: any) => {
-          this.apiGetPokemons(resPokemons.url)
+          this.apiGetPokemon(resPokemons.url)
           .subscribe(
             res => resPokemons.status = res
         )
@@ -25,7 +25,7 @@ export class PokeApiService {
       })
     )
   }
-  public apiGetPokemons(url: string): Observable<any> {
+  public apiGetPokemon(url: string): Observable<any> {
     return this.http.get<any>(url).pipe(
       map(res => res))
   }
